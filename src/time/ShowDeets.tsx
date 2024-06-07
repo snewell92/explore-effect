ae636c8a0ccc274546f93603d8faf0640dd76a14
Sean Newell <sean@thenewells.us>
2024-06-07 14:36:16 +0100

    todo ignore me

---
+++ src/time/ShowDeets.tsx
@@ -0,0 +1,13 @@
+import { Today } from "./time";
+
+interface DeetsProps {
+  today: Today;
+}
+
+export const ShowDeets = ({}: DeetsProps) => {
+  return (
+    <div>
+      <p>Hai</p>
+    </div>
+  );
+};
